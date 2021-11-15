41934995b8e8870c4a09cec3de1f8a1c35b9e29c
elizeubarbosaabreu <elizeubcorreios@gmail.com>
2021-11-14 21:07:17 -0400

    Add threads.py and update for README.md

--- threads.py
+++ threads.py
@@ -1,28 +1,19 @@
 import time
 from threading import Thread
 
-def car_1(speed):
+def car_1(speed, pilot):
     route = 0
     while route <= 100:
-        print(f'The <car number 1> is in {route} Km...')
+        print(f'The {pilot} is in {route} Km...')
         route += speed
         time.sleep(1)
-    print('    End for <car number 1>')
-        
-def car_2(speed):
-    route = 0
-    while route <= 100:
-        print(f'The <car number 2> is in {route} Km...')
-        route += speed
-        time.sleep(1)
-    print('    End for <car number 2>')
+    print(f'    End for {pilot}...')
 
-# car_1(10)
-# car_2(25)
 
-t_car_1 = Thread(target=car_1, args=[15])
-t_car_2 = Thread(target=car_2, args=[25])
+t_car_1 = Thread(target=car_1, args=[25, 'Michaek Schumacher'])
+t_car_2 = Thread(target=car_1, args=[20, 'Rubinho Barrichello'])
+t_car_3 = Thread(target=car_1, args=[50, 'Airton Senna'])
 
 t_car_1.start()
 t_car_2.start()
-        
+t_car_3.start()
