2adf43fac48e1aabeaa19119230c107e49662f8d
Elizeu Barbosa Abreu <elizeubcorreios@gmail.com>
2023-06-23 18:55:07 -0400

    Update webscraping.py

--- webscraping.py
+++ webscraping.py
@@ -7,9 +7,9 @@ site = requests.get("https://www.remessaonline.com.br/cotacao/cotacao-dolar").co
 soup = BeautifulSoup(site, 'html.parser')
 
 # prettify transforma o html em string
-print(soup.prettify())
+# print(soup.prettify())
 
-Dolar = soup.find("div", class_="style__Text-sc-15flwue-2 cSuXFv")
+Dolar = soup.find("div", class_="style__Text-sc-1a6mtr6-2 ljisZu")
 title = soup.title
 # print(title.string) 
 print(f'Valor do Dolar comercial hoje: R$ {(Dolar.string).replace(" Reais", "...")}')
